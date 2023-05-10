ce77b0959f4ba19a4669bb573da0fce51c34e4df
Luiggi <luduve97@gmail.com>
2023-05-10 12:48:20 +0200

    created first component

---
+++ src/components/Escena.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { data } from '../constants';
+
+const Escena = () => {
+  return (
+    <>
+    {
+      // data.map(item => )
+    }
+        <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+    </>
+  )
+}
+
+export default Escena
---
+++ src/constants/index.tsx
@@ -0,0 +1,6 @@
+import data from "./data"
+
+
+export{
+    data
+}
